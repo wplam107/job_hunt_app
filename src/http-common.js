6da0695db16f9168003c5359aebd03e5eff54a3c
Wayne <w.p.lam107@gmail.com>
2021-06-24 21:29:36 -0400

    containerized

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8080/api",
+  // baseURL: "http://localhost:8080/api",
+  baseURL: "http://127.0.0.1:8000/api",
   headers: {
     "Content-type": "application/json"
   }
